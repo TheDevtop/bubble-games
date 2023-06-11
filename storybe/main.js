93a7ee447614131ba70052ef73b20bbcf29fe75a
Thijs Haker <theage2001@gmail.com>
2023-06-11 10:35:15 +0200

    Added /storyTypes

--- storybe/main.js
+++ storybe/main.js
@@ -1,6 +1,18 @@
 const express = require('express')
+const fs = require('fs');
 const app = express()
 const port = 8080
+
+app.get('/storyTypes', (req, res) => {
+
+fs.readFile('/Users/joe/test.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  res.send(data);
+});
+})
 
 app.use('/whatavue', express.static('static'))
 
